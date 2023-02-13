bc1348a25bdfcb59687ecf438feba54870b65bde
Svetlana Maslennikova <svetlana-pavlova1005@mail.ru>
2023-02-13 13:00:56 +0300

    lesson 11

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -69,6 +69,26 @@ class App extends React.Component {
       return headers
   }
 
+  createBook(name, author) {
+      const headers = this.get_headers()
+      const data = {name: name, author:author}
+      axios.post('http://127.0.0.1:8000/api/books/', data, {headers})
+          .then(response => {
+              let new_book = response.data
+              const author = this.state.authors.filter((item) => item.id === new_book.author)[0]
+              new_book.author = author
+              this.setState({books: [...this.state.books, new_book]})
+          }).catch(error => console.log(error))
+  }
+
+  deleteBook(id) {
+      const headers = this.get_headers()
+      axios.delete('http://127.0.0.1:8000/api/books/${id}', {headers})
+          .then(response => {
+              this.setState({books: this.state.books.filter((item)=>item.id !== id)})
+          }).catch(error => console.log(error))
+  }
+
   load_data() {
       const headers = this.get_headers()
       axios.get('http://127.0.0.1:8000/api/authors', {headers})
@@ -116,7 +136,8 @@ class App extends React.Component {
               </nav>
               <Switch>
                   <Route exact path='/' component={() => <AuthorList authors={this.state.authors} />} />
-                  <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+                  <Route exact path='/books/create' component={() => <BookForm authors={this.state.authors} createBook={(name, author)=>this.createBook(name, author)} />} />
+                  <Route exact path='/books' component={() => <BookList items={this.state.books} deleteBook={(id)=>this.deleteBook(id)} />} />
                   <Route exact path='/author/:id' component={() => <AuthorBookList items={this.state.books} />} />
                   <Route exact path='/login' component={() => <LoginForm get_token={(login, password) => this.get_token(login, password)}/>} />
                   <Redirect from='/authors' to='/'/>
